634b46c820a9ca61fed09d5604f2aec06fbff458
Siddharth Pratap Singh <siddharthpratap92@gmail.com>
2017-09-29 12:24:26 -0400

    Bubble Sort Implementation

--- bubbleSort.py
+++ bubbleSort.py
@@ -1,10 +1,22 @@
 def bubbleSort(l):
+    n=len(l)
     for i in range(len(l)):
         m=i
-        for j in range(len(l)):
+        swappped=False
+        for j in range(l0,n-i-1)):
             if l[m]<l[j]:
                 l[m],l[j]=l[j],l[m]
+            swapped=True
+        if swapped==False:
+            break
     print (l)
 
 arr=[int(x)for x in input().split()]
 bubbleSort(arr)
+
+'''
+Best Case: No swap occurs in inner loop.
+Worst and Average= O(n^2)
+Stable
+In-place
+'''
