af65ed268d43c0f8f9f94313dd06a882fb452db6
Siddharth Pratap Singh <siddharthpratap92@gmail.com>
2017-10-21 21:51:09 -0400

    Update question1.py

--- Random Questions/Udacity/question1.py
+++ Random Questions/Udacity/question1.py
@@ -20,10 +20,21 @@ def question1(s,t):
                 return True
     return False
 
-
-
-
-
+def question(s,t):
+    s=s.lower() #Convert both strings to their lower cases
+    t=t.lower()
+    #create counter for pattern
+    ct=Counter(t)
+    #a list to find the indexes of the match in text
+    l=[]
+    for i in range(len(s)-len(t)+1): #O(n-m)
+        #find substring
+        sub_s=s[i:i+len(t)]
+        cs=Counter(sub_s) #O(m)
+        #match this substring counter with the pattern
+        if cs==ct: #O(m)
+            l.append(i)
+    return l
 
 s=input("Enter the main string: ")
 t=input("Enter the string to check in main string: ")
